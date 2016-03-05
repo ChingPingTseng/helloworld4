864ce5dd1968916816ed35e9bdd9e0ac771f10fc
ChingPingTseng <davidtseng200@gmail.com>
2016-03-05 20:37:54 +0800

    Initial Commit

---
+++ helloworld4/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  helloworld4
+//
+//  Created by ChingPing Tseng on 3/5/16.
+//  Copyright © 2016 ChingPingTseng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
